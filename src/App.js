20ef841fd4254cd3f5fe31052a7cc4de3de01080
Mariyq1 <m4697597@gmail.com>
2023-03-03 19:55:28 +0100

    Create comp Header,Sort,Categories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import Header from './Components/Header';
+import Categories from './Components/Categories';
+import Sort from './Components/Sort';
 import './scss/app.scss';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div class="wrapper">
+        <Header/>
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories/>
+            <Sort/>
+          </div>
+          <h2 class="content__title">All sushi</h2>
+        </div>
+      </div>
+    </div>
       
     </div>
   );
---
+++ src/Components/Categories.jsx
@@ -0,0 +1,15 @@
+function Categories (){
+  return(
+  <div class="categories">
+    <ul>
+      <li class="active">All</li>
+      <li>Maki</li>
+      <li>Sashimi</li>
+      <li>Nigiri</li>
+      <li>Sushi Pizza</li>
+      <li>Combo</li>
+      </ul>
+  </div>
+  )
+}
+export default Categories;
