0446b0795b751941f941f3e2f75833bcaa81f27f
Mariyq1 <m4697597@gmail.com>
2023-03-04 23:43:46 +0100

    Create request to back using fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Categories from './Components/Categories';
 import Sort from './Components/Sort';
 import './scss/app.scss';
 import SushiBlock from './Components/SushiBlock';
-import sushi from './assets/sushi.json';
+
 
 function App() {
+  const [items, setItems]=useState([]);
+  useEffect(()=>{
+    fetch('https://6403a4573bdc59fa8f2a3657.mockapi.io/items')
+    .then((res)=>{
+      return res.json();
+    })
+    .then((arr)=>{
+      setItems(arr)
+    })
+  },[])
   return (
     <div className="App">
       <div className="wrapper">
@@ -20,11 +31,10 @@ function App() {
           <h2 className="content__title">All sushi</h2>
           <div className='content__items'>
           {
-            sushi.map((obj)=>(
+            items.map((obj)=>(
               <SushiBlock {...obj} key={obj.id}/>
             ))
           }
-          
           </div>
         </div>
       </div>
